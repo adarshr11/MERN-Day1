afa8919740d71f5595c1e5fe316ad3db6ddd626e
adarshr11 <132336582+adarshr11@users.noreply.github.com>
2023-05-02 15:49:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+// let a=10;
+// let b="10";
+// if(a===b)
+// {
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+// let f="adarsh";
+// let g="ravindran";
+// let h=f+g;
+// console.log(`${f}  ${g}`);
+// //
+// let car={
+//     model:"austin",color:"white",price:10000000
+
+// }
+// console.log(car);
+// console.log(car.color);
+// console.log(`${car.model}  ${car.color}`);
+// let person={
+//     name:"adarsh ravndran",
+//     college:"cce",
+//     class:"cse s4",
+//     address:{
+//        housename: "thommana house",
+//        street:"fzzff"}
+// }
+// console.log(`${person.name}  ${person.address.housename}`);
+// //array
+// let fruits=["apple","mango","dates",10]
+// fruits[4]="grapes";
+// fruits.unshift("pineapple");
+// console.log(fruits)
+// fruits.pop();
+// console.log(fruits)
+// function addition(a,b){
+//     console.log(a+b);
+
+// }
+// function sub(a,b)
+// {
+//     console.log(a-b);
+// }
+// addition(10,20);
+// sub(20,300);
+// multi = (a,b) =>
+// {
+//     console.log(a*b);
+// }
+// multi(10,20);
+// add = (a,b) =>
+// {
+//   let d =a+b;
+//   if( a>10)
+//   {
+//     console.log("a is greater");
+//   }
+//   else{
+//     console.log("a is smaller");
+//   }
+// }
+// add(10,10);
+// let numbers =[1,2,3,4,5,6,7,8,9,10];
+// print=(a)=>
+// {
+//     for(i=0;i<=9;i++)
+//     {
+//         console.log(a[i]);
+//     }
+// }
+// print(numbers);
+// let numbers =[1,2,3,4,5,6,7,8,9,10];
+// print=(a)=>
+// { i=0;
+//     while(i<=9)
+//     {
+//         console.log(a[i]);
+//         i++;
+//     }
+// }
+// print(numbers);
+let numbers =[1,2,3,4,5,6,7,8,9,10];
+print=(a)=>
+{ i=0;
+    do
+    {
+        console.log(a[i]);
+        i++;
+    }while(i<=9);
+}
+print(numbers);
